b84db25a5841627371cbcaec9d9b71bdd1062726
Brent Clancy <eternialogic@gmail.com>
2022-12-30 16:13:42 -0500

    Update Win11Fixes.bat

--- Win11Fixes.bat
+++ Win11Fixes.bat
@@ -1,13 +1,5 @@
 @echo off
-rem ================================================================================
-rem  Windows 11 Requirements Bypass
-rem ================================================================================
-rem
-rem  Written by Shiro39
-rem  Modified by Eternialogic
-rem  Last modified 2022-12-30
-rem
-rem ================================================================================
+pause
 title Windows 11
 mode con:cols=80 lines=20
 
@@ -89,21 +81,17 @@ echo Adding key "BypassSecureBootCheck" into registry...
 reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassSecureBootCheck" /f /t REG_DWORD /d 1 > nul
 echo Adding additional key "AllowUpgradesWithUnsupportedTPMOrCPU" into registry...
 reg add "HKLM\SYSTEM\Setup\MoSetup" /v "AllowUpgradesWithUnsupportedTPMOrCPU" /f /t REG_DWORD /d 1 > nul
-
+echo.
+echo special fixes... Context Menu, Search Settings, Bing Search, Cortana
+pause
 echo Adding Context Menu fix "InprocServer32" into registry...
 reg add HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32 /ve /d "" /f
-
-echo Disabling Bing Search in Start Menu
-if( -not (Test-Path -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer)){
-New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer
-}
-Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer -Name "DisableSearchBoxSuggestions" -Value 1 -Type DWORD
-
 echo Disabling Search Highlights.
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\SearchSettings" /v "IsDynamicSearchBoxEnabled" /d 0 /t REG_DWORD /f
-
-
+echo Disabling Bing Search in Start Menu
+reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Explorer" /v "DisableSearchBoxSuggestions" /f /t REG_DWORD /d 1
+echo Disabling Cortana
+reg add "Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v "AllowCortana" /f /t REG_DWORD /d 0
 echo.
-echo Press any key to close this CMD...
-pause > nul
-exit
+echo Please restart to make these changes.
+pause
